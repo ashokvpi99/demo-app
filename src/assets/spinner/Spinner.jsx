c7a5cfbe8326cfa117df8c4053070250ff511ef2
ashokvpi99 <ashokvpi99@gmail.com>
2018-10-02 19:04:43 +0530

    Spinner added

--- src/assets/spinner/Spinner.jsx
+++ src/assets/spinner/Spinner.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Loader from 'react-loaders';
-import 'spinner.scss';
+import 'Spinner.scss';
 
-const Spinner = (props) => <Loader type="square-spin" />;
+const Spinner = (props) => <Loader type="square-spin" active={props.isLoading}/>;
 
 export default Spinner;
